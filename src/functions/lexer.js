c46436c6889b7978efcd1e5569688577c1231313
Pavel Bobylyev <pbobylyev@appliedtech.ru>
2020-02-01 22:54:49 +0500

    simple commands added to lexer

--- src/functions/lexer.js
+++ src/functions/lexer.js
@@ -1,8 +1,19 @@
 export default function lexer(arr) {
   return arr.reduce(
     (acc, cur) => {
-      const command = cur.split(' ');
-      acc.push(command);
+      const line = cur.split(' ').filter(el => el !== '');
+      const command = line[0].toLowerCase();
+      const times = parseInt(line[1], 10);
+      if (isNaN(times)) return acc;
+      if (['влево', 'left'].includes(command)) {
+        acc.push(['left', times]);
+      } else if (['вправо', 'right'].includes(command)) {
+        acc.push(['right', times]);
+      } else if (['вверх', 'up'].includes(command)) {
+        acc.push(['up', times]);
+      } else if (['вниз', 'down'].includes(command)) {
+        acc.push(['down', times]);
+      }
       return acc;
     },
     []
