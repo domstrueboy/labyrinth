f0d82b72d22d0d25170b77b29643613fc86ec85f
Pavel Bobylyev <pbobylyev@appliedtech.ru>
2020-02-15 17:22:33 +0500

    wip

--- src/functions/runner.js
+++ src/functions/runner.js
@@ -1,34 +1,23 @@
+const MAX_COMMANDS_LEN = 1000;
+
 export default function runner(arr) {
-  return arr.reduce(
-    (acc, cur) => {
-      if (Array.isArray(cur)) {
-        const command = cur[0];
-        const times = cur[1];
-        for (let i = 0; i < times; i++) {
-          acc.push(command);
-        }
+  const len = arr.length;
+  const newArr = [];
+  let index = 0;
+  while (index < len && index < MAX_COMMANDS_LEN) {
+    const cur = arr[index];
+    if (Array.isArray(cur)) {
+      const command = cur[0];
+      const times = cur[1];
+      if (command === 'repeat') {
+        const buf = [];
+        
       }
-      // if (cur === 'delimeter') {
-      //   acc.push(cur);
-      //   return acc;
-      // }
-      // const line = cur.split(' ').filter(el => el !== '');
-      // const command = line[0].toLowerCase();
-      // const times = parseInt(line[1], 10);
-      // if (isNaN(times)) return acc;
-      // if (['влево', 'left'].includes(command)) {
-      //   acc.push(['left', times]);
-      // } else if (['вправо', 'right'].includes(command)) {
-      //   acc.push(['right', times]);
-      // } else if (['вверх', 'up'].includes(command)) {
-      //   acc.push(['up', times]);
-      // } else if (['вниз', 'down'].includes(command)) {
-      //   acc.push(['down', times]);
-      // } else if (['повтор', 'повторить', 'repeat'].includes(command)) {
-      //   acc.push(['repeat', times]);
-      // }
-      return acc;
-    },
-    []
-  );
+      for (let i = 0; i < times; i++) {
+        newArr.push(command);
+      }
+    }
+    index++;
+  }
+  return newArr;
 }
