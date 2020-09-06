da1872aafe9a575f7a7218b114bdababd4b24de1
Pavel Bobylyev <pbobylyev@appliedtech.ru>
2020-09-06 13:56:31 +0500

    saving current level added

--- src/stores.js
+++ src/stores.js
@@ -4,6 +4,12 @@ export const content = writable('');
 
 export const commands = writable([]);
 
-export const level = writable(1);
+export const level = writable(
+  parseInt(localStorage.getItem('level'), 10) || 1,
+);
+
+level.subscribe(value => {
+	localStorage.setItem('level', value);
+});
 
 export const status = writable(null); // null | started | win | lose
